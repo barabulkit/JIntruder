7c962bd638f095931904334d72f035ae80511436
EleshNorn <densolo777@gmail.com>
2023-02-15 22:11:37 +0300

    Changed cluster bomb stub to stub without arrays

--- src/main/java/utils/AttackPerformer.java
+++ src/main/java/utils/AttackPerformer.java
@@ -47,25 +47,23 @@ public class AttackPerformer {
         ArrayList<String> result = new ArrayList<String>();
 
         if(options.size() < positions.size()) return result;
-        ArrayList<String> list = new ArrayList<String>();
-        list.add(request);
-        for(int i = 0; i < positions.size(); i++) {
-            list = fillPosition(list, positions.get(i), options.get(i));
-        }
+        printAll(request, 0, positions, options, 0);
 
-        System.out.println(list.size());
         return result;
     }
 
-    public ArrayList<String> fillPosition(ArrayList<String> requests, Position position, DefaultListModel options) {
-        ArrayList<String> result = new ArrayList<String>();
-        for(String request : requests) {
-            for(int i = 0; i < options.size(); i++) {
-                StringBuffer buffer = new StringBuffer(request);
-                buffer.replace(position.getStartIndex(), position.getEndIndex(), (String) options.getElementAt(i));
-                result.add(buffer.toString());
+    public void printAll(String request, int positionNum, ArrayList<Position> positions, ArrayList<DefaultListModel> options, int offset) {
+        Position position = positions.get(positionNum);
+        for(int i = 0; i < options.get(positionNum).size(); i++) {
+            StringBuilder editor = new StringBuilder(request);
+            String replacer = (String) options.get(positionNum).getElementAt(i);
+            int newOffset = offset - (position.getEndIndex() - position.getStartIndex()) + replacer.length();
+            editor.replace(position.getStartIndex()+offset, position.getEndIndex()+offset, replacer);
+            if(positionNum != positions.size() - 1) {
+                printAll(editor.toString(), positionNum + 1, positions, options, newOffset);
+            } else {
+                System.out.println(editor.toString());
             }
         }
-        return result;
     }
 }
